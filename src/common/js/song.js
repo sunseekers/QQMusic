7ba9c3c7f1bcc9bb5246ff2471882c93e8546ff5
sunseekers <1390639401@qq.com>
2018-05-23 07:57:57 +0800

    song 类的封装

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -2,36 +2,74 @@ import {getLyric} from 'api/song'
 import {ERR_OK} from 'api/config'
 import {Base64} from 'js-base64'
 
+// export default class Song {
+//   constructor({id, mid, singer, name, album, duration, image, url}) {
+//     this.id = id
+//     this.mid = mid
+//     this.singer = singer
+//     this.name = name
+//     this.album = album
+//     this.duration = duration
+//     this.image = image
+//     this.url = url
+//   }
+
+//   getLyric() {
+//     if (this.lyric) {
+//       return Promise.resolve(this.lyric)
+//     }
+
+//     return new Promise((resolve, reject) => {
+//       getLyric(this.mid).then((res) => {
+//         if (res.retcode === ERR_OK) {
+//           this.lyric = Base64.decode(res.lyric)
+//           resolve(this.lyric)
+//         } else {
+//           reject('no lyric')
+//         }
+//       })
+//     })
+//   }
+// }
+
+// export function createSong(musicData) {
+//   return new Song({
+//     id: musicData.songid,
+//     mid: musicData.songmid,
+//     singer: filterSinger(musicData.singer),
+//     name: musicData.songname,
+//     album: musicData.albumname,
+//     duration: musicData.interval,
+//     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
+//     url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+//   })
+// }
+
+// function filterSinger(singer) {
+//   let ret = []
+//   if (!singer) {
+//     return ''
+//   }
+//   singer.forEach((s) => {
+//     ret.push(s.name)
+//   })
+//   return ret.join('/')
+// }
+
 export default class Song {
-  constructor({id, mid, singer, name, album, duration, image, url}) {
+  //设计类，代码集中一个地方类便于维护，不用去写大量的代码，类的扩展性强面向对象
+  constructor({id,mid,singer,name,album,duration,image,url}) {
     this.id = id
     this.mid = mid
     this.singer = singer
     this.name = name
-    this.album = album
-    this.duration = duration
+    this.album = album 
+    this.duration = duration 
     this.image = image
     this.url = url
   }
-
-  getLyric() {
-    if (this.lyric) {
-      return Promise.resolve(this.lyric)
-    }
-
-    return new Promise((resolve, reject) => {
-      getLyric(this.mid).then((res) => {
-        if (res.retcode === ERR_OK) {
-          this.lyric = Base64.decode(res.lyric)
-          resolve(this.lyric)
-        } else {
-          reject('no lyric')
-        }
-      })
-    })
-  }
 }
-
+//这是一个工厂方法，不直接调用new，他提供提供一个方法去返回实力
 export function createSong(musicData) {
   return new Song({
     id: musicData.songid,
@@ -41,18 +79,16 @@ export function createSong(musicData) {
     album: musicData.albumname,
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-    url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
-  })
+    url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`    
+  })  
 }
-
-function filterSinger(singer) {
+function filterSinger (singer){
   let ret = []
   if (!singer) {
-    return ''
+    return ""
   }
-  singer.forEach((s) => {
+  singer.forEach(s => {
     ret.push(s.name)
   })
   return ret.join('/')
 }
-
--- src/components/singerDetail/singerDetail.vue
+++ src/components/singerDetail/singerDetail.vue
@@ -5,14 +5,45 @@
 </template>
 <script>
 import {mapGetters} from 'vuex'
+import { getSingerDetail} from "api/singer"
+import {ERR_OK} from "api/config"
+import { createSong} from "common/js/song"
 export default {
+  data(){
+    song:[]
+  },
   computed:{
     ...mapGetters([
       'singer'
       ])
   },
   created(){
-    console.log(this.singer)
+    //console.log(this.singer)
+    this._getDetail(this.singer)
+  },
+  methods:{
+    _getDetail(){
+      if (!this.singer.id){
+        this.$router.push('/singer')
+        return
+      }
+      getSingerDetail(this.singer.id).then(res=>{
+        if (res.code ===ERR_OK){
+          this.songs = this._normalizeSongs(res.data.list)
+          console.log(12)
+          console.log(this.songs)
+        }
+      })
+    },
+    _normalizeSongs(list){
+      let ret = []
+      list.forEach(item => {
+        let { musiData} = item
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+    }
   }
 }
 </script>
